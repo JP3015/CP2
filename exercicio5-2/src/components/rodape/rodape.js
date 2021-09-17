5232c9c3d14283637d5523f2db6c9a395b8877fe
Rasantis <76708662+Rasantis@users.noreply.github.com>
2021-09-17 19:48:10 -0300

    commit 2 do rodapé

--- exercicio5-2/src/components/rodape/rodape.js
+++ exercicio5-2/src/components/rodape/rodape.js
@@ -3,9 +3,9 @@ import React from 'react'
 export default function Rodape() {
     return(
 
-        <section>
-            <h1>RODAPE</h1>
-        </section>
+        <footer>
+            <p><b>Endereço:</b> Av. Paulista, 1106-São Paulo</p>
+        </footer>
 
     )
 }
