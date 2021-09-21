d1762c741bb69b0d3bb375770ed81a321f4c35e2
JP <80105602+JP3015@users.noreply.github.com>
2021-09-20 22:13:34 -0300

    mudando o cabeçalho e o corpo

--- exercicio5-2/src/components/cabecalho/cabecalho.js
+++ exercicio5-2/src/components/cabecalho/cabecalho.js
@@ -3,9 +3,10 @@ import React from 'react'
 export default function Cabecalho() {
     return(
 
-        <section>
-            <h1>CABEÇALHO</h1>
-        </section>
+        <header>
+            <h1>Sorveteria Go Code FIAP</h1>
+            <p> Nós somos a sorveteria Go Code Fiap, a sorveteria com preços especiais para os alunos do curso de ADS</p>
+        </header>
 
     )
 }
--- exercicio5-2/src/components/corpo/corpo.js
+++ exercicio5-2/src/components/corpo/corpo.js
@@ -3,9 +3,32 @@ import React from 'react'
 export default function Corpo() {
     return(
 
-        <section>
-            <h1>CORPO</h1>
-        </section>
+        <body>
+            <h2>Veja a nossa lista de sabores</h2>
+            <ul>
+                <li>Chocolate</li> 
+                <li>Morango</li> 
+                <li>Creme</li> 
+                <li>Flocos</li> 
+                <li>Doce de Leite</li> 
+                <li>Ninho Trufado</li> 
+                <li>Milho Verde</li> 
+                <li>Napolitano</li>
+            </ul>
+            
+            <h2>Veja a nossa lista de acompanhamentos</h2>
+
+            <ul>
+                <li>Farofa doce</li> 
+                <li>Nutella</li> 
+                <li>Leite condensado</li> 
+                <li>Amendoim</li> 
+                <li>M&M's</li> 
+                <li>Leite em pó</li> 
+                <li>Granulado de chocolate</li>
+                <li>Banana</li>
+            </ul>
+        </body>
 
     )
 }
