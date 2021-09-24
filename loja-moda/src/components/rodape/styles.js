e4947a4f65d220b980f0d3009306b84c4c526fcb
kacsl <79801965+kacristhine@users.noreply.github.com>
2021-09-24 09:21:42 -0300

    Adicionado home

---
+++ loja-moda/src/components/rodape/styles.js
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  display: flex;
+  justify-content: center;
+`;
+
+export const RodapeSection = styled.b`
+  font-size: 10px;
+  line-height: 17px;
+  color: "#000";
+`;
